0b549ff9dcf371815ce887513234f9d269192a4c
Loïc Hoguin <essen@ninenines.eu>
2026-05-11 12:22:22 +0200

    Fix outdated Gun package description

--- index/gun.mk
+++ index/gun.mk
@@ -1,6 +1,6 @@
 PACKAGES += gun
 pkg_gun_name = gun
-pkg_gun_description = Asynchronous SPDY, HTTP and Websocket client written in Erlang.
+pkg_gun_description = HTTP/1.1, HTTP/2, Websocket client (and more) for Erlang/OTP.
 pkg_gun_homepage = http//ninenines.eu
 pkg_gun_fetch = git
 pkg_gun_repo = https://github.com/ninenines/gun
